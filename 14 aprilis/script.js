5b21636e5afbd32ee4d30ef47329d56504c08b96
plumegravi <95850126+plumegravi@users.noreply.github.com>
2022-04-21 08:40:27 +0300

    Add files via upload

---
+++ 14 aprilis/script.js
@@ -0,0 +1,15 @@
+function sasveicinaties(){
+    alert("Welcome to my shithole");
+}
+function CheckUsernamePassword(){
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    if(username== "demo" && password =="demo"){
+        alert("Log in successful.")
+    }
+    else{
+        alert("Access denied")
+    }
+}
+
+
