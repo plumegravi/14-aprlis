afe36a3f4de914ce7ac898b456d8b1ca5416b272
plumegravi <95850126+plumegravi@users.noreply.github.com>
2022-04-21 09:28:20 +0300

    Add files via upload

---
+++ web calculator/script.js
@@ -0,0 +1,6 @@
+function AprekinatSummu(){
+    let firstNumber = document.getElementById("FirstNumber").value;
+    let secondNumber = document.getElementById("SecondNumber").value;
+    let result = firstNumber + secondNUmber;
+    alert("Aprēķinu rezultāts ir " + result);
+}
